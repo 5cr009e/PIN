ac6fc2095f707bb607e8df9131c7209537d01e8b
Linjun Li <linjunlee@gmail.com>
2020-08-05 03:55:19 -0700

    update

--- src/main.js
+++ src/main.js
@@ -2,13 +2,9 @@ const {app, BrowserWindow} = require('electron')
 const url = require('url')
 const path = require('path')
 const readline = require('readline');
-const { Console } = require('console');
-
-
-let win
-
-function createWindow(){
-    win = new BrowserWindow({
+let winQueue = []
+function createWindow(BrowserWindow){
+    let win = new BrowserWindow({
         width: 400,
         height: 600,
         frame: false,
@@ -27,35 +23,45 @@ function createWindow(){
     return win
 }
 
+function appendWindow(){
+    winQueue.push(createWindow(BrowserWindow))
+}
+
+function removeWindow(){
+    winQueue[winQueue.length-1].close()
+    winQueue.pop()
+}
+
+
 function main(){
     console.log('Welcome to use PIN. Press h for more info.')
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: 'PIN>>>'
-    })
-    winQueue = []
-
-    rl.prompt()
-    rl.on('line', (line) => {
-        switch(line){
-            case 'new':
-                winQueue.push(createWindow())
-                rl.prompt()
-                break
-            case 'rm':
-                winQueue[winQueue.length-1].close()
-                winQueue.pop()
-                rl.prompt()
-                break
-            case 'q':
-                process.exit(0)
-            default:
-                console.log(`Unrecognized command ${line}`)
-        }
-    }).on('close', () => {
-        console.log('Bye!')
-    })
+    if (process.platform == "win32") {
+        console.log('On win32 platform, cli is not support.')
+        appendWindow()
+    } else {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            prompt: 'PIN>>>'
+        })
+        rl.prompt()
+        rl.on('line', (line) => {
+            switch(line){
+                case 'new':
+                    appendWindow()
+                    rl.prompt()
+                    break
+                case 'rm':
+                    removeWindow()
+                    rl.prompt()
+                    break
+                case 'q':
+                    console.log("Bye!")
+                    process.exit(0)
+                default:
+                    console.log(`Unrecognized command ${line}`)}})
+    }
+   
 }
 
 app.on('ready', main)
